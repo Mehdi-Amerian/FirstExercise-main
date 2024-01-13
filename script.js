37b17fe68a7847f7700edf93bd25f9431f09ae1f
El Mahdi Amerian <emaam002@edu.xamk.fi>
2024-01-13 17:48:37 +0200

    Dynamic date in footer

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-document.getElementById("actionButton").addEventListener("click", function() {
+document.getElementById("button").addEventListener("click", function() {
     alert("Button clicked!");
 });
+
+//Get the current year
+function getCurrentYear() {
+    return new Date().getFullYear();
+}
+
+//Set the current year in the footer
+function setDynamicYear() {
+    var currentYearSpan = document.getElementById('current-year');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = getCurrentYear();
+    }
+}
+
+//Set the dynamic year when the page loads
+window.onload = function () {
+    setDynamicYear();
+};
